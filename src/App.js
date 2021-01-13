7a0af18cd77c2a857855f3010be4186efd3a8a96
Osaiteh <ogloriousgloo@gmail.com>
2021-01-13 17:37:35 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,6 +13,8 @@ import Allreducers from '../src/component/reducers'
 import Dashboard from './component/Dashboard';
 import Mailbox from './component/Mailbox';
 import Appp from './component/templateeditor';
+import Users from './component/user-management';
+import AdminDashboard from './component/admindashboard';
 import selectTemp from './component/createemailTep/createemailTem2';
 
 
@@ -20,22 +22,24 @@ import selectTemp from './component/createemailTep/createemailTem2';
 
 function App() {
   return (
-<Provider store={store} >
-<Router>
-     <Switch>
-       <Route exact path="/" component= {LP}/>
-       <Route exact path="/about" component= {About }/>
-       <Route exact path="/pricing" component= {Pricing }/>
-       <Route exact path="/contact" component= {Contactt }/>
-       <Route exact path="/signup" component= {Signup }/>
-       <Route exact path="/services" component= {Services }/>
-       <Route  path="/dashboard" component= {Dashboard }/>
-       <Route  path="/Mailbox" component= {Mailbox }/>
-       <Route  path="/Templateeditor" component= { Appp }/>
-       <Route  path="/selecttemp" component= { selectTemp }/>
-     </Switch>
-       </Router>
-       </Provider>
+    <Provider store={store}>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={LP} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/pricing" component={Pricing} />
+          <Route exact path="/contact" component={Contactt} />
+          <Route exact path="/signup" component={Signup} />
+          <Route exact path="/services" component={Services} />
+          <Route path="/dashboard" component={Dashboard} />
+          <Route path="/admin/users" component={Users} />
+          <Route path="/dashboardd" component={AdminDashboard} />
+          <Route path="/Mailbox" component={Mailbox} />
+          <Route path="/Templateeditor" component={Appp} />
+          <Route path="/selecttemp" component={selectTemp} />
+        </Switch>
+      </Router>
+    </Provider>
   );
 }
 export default App;
