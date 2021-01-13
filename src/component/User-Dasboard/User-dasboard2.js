fb7a0b88f5a214da2ba2e409d363f1269bdd3a6a
Malik <malikmukhtar63@gmail.com>
2021-01-13 19:26:27 +0100

    added chart.js to the dashboard

--- src/component/User-Dasboard/User-dasboard2.js
+++ src/component/User-Dasboard/User-dasboard2.js
@@ -1,41 +1,140 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { Bar, Line, Pie } from 'react-chartjs-2';
 
-import React from 'react'
-import {Link} from 'react-router-dom';
-
-export default function Userdasbord2() {
+export default class Userdasbord2 extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      chartData: {
+        labels: [
+          'January',
+          'February',
+          'March',
+          'April',
+          'May',
+          'June',
+          'July',
+          'August',
+          'September',
+          'October',
+          'November',
+          'December',
+        ],
+        datasets: [
+          {
+            label: 'Reviews',
+            data: [
+              1000,
+              2000,
+              1500,
+              3000,
+              2500,
+              1800,
+              1700,
+              2000,
+              1500,
+              3000,
+              2500,
+              1800,
+            ],
+            backgroundColor: [
+              'rgba(255,99,132,0.6)',
+              'rgba(54,162,123,0.6)',
+              'rgba(255,206,86,0.6)',
+              'rgba(75,192,192,0.6)',
+              'rgba(153,102,255,0.6)',
+              'rgba(255,159,64,0.6)',
+              'rgba(54,162,123,0.6)',
+              'rgba(255,99,132,0.6)',
+              'rgba(75,192,192,0.6)',
+              'rgba(255,206,86,0.6)',
+              'rgba(255,159,64,0.6)',
+              'rgba(153,102,255,0.6)',
+            ],
+          },
+        ],
+      },
+      contactData: {
+        labels: ['Oct', 'Nov', 'Dec', 'Jan'],
+        datasets: [
+          {
+            label: 'Total Contacts',
+            data: [1000, 1500, 2000, 3000],
+            backgroundColor: [
+              'rgba(255,159,64,0.6)',
+              'rgba(54,162,123,0.6)',
+              'rgba(255,99,132,0.6)',
+              'rgba(75,192,192,0.6)',
+            ],
+          },
+        ],
+      },
+      audienceData: {
+        labels: ['Oct', 'Nov', 'Dec', 'Jan'],
+        datasets: [
+          {
+            label: 'Audience Reached',
+            data: [1000, 3500, 5000, 6000],
+            backgroundColor: [
+              'rgba(54,162,123,0.6)',
+              'rgba(255,159,64,0.6)',
+              'rgba(75,192,192,0.6)',
+              'rgba(255,99,132,0.6)',
+            ],
+          },
+        ],
+      },
+      designData: {
+        labels: ['Oct', 'Nov', 'Dec', 'Jan'],
+        datasets: [
+          {
+            label: 'Audience Reached',
+            data: [1000, 3500, 5000, 6000],
+            backgroundColor: [
+              'rgba(54,162,123,0.6)',
+              'rgba(255,159,64,0.6)',
+              'rgba(75,192,192,0.6)',
+              'rgba(255,99,132,0.6)',
+            ],
+          },
+        ],
+      },
+    };
+  }
+  render() {
     return (
-        <div style={{width: '60%', backgroundColor: '#F4F5F7', margin: 'auto', minHeight: '100vh'}}>
-
+      <div
+        style={{
+          width: '60%',
+          backgroundColor: '#F4F5F7',
+          margin: 'auto',
+          minHeight: '100vh',
+        }}
+      >
         <div className="row p-4 dashboardrow">
           <div className="col">
             <h3 className="active">Dashboard</h3>
           </div>
-          {/* <div className="col" id="date" style={{fontSize: 12, margin: 10}}>
-          </div> */}
+          
         </div>
         <div className="row p-4">
           <div className="col card">
-            <h6>Total Contact</h6>
-            <canvas id="pie-chart" style={{display: 'block', width: 50, height: 40}}>
-  </canvas>
+            <Line data={this.state.contactData} options={{}} />
           </div>
           <div className="col card">
-            <h6>Audience Reached</h6>
-            <canvas id="pie-chart2" style={{display: 'block', width: 50, height: 40}}>
-  </canvas>
+            <Bar data={this.state.audienceData} options={{}} />
           </div>
           <div className="col card">
-            <h6>Total Design</h6>
-            <canvas id="pie-chart3" style={{display: 'block', width: 50, height: 40}}>
-  </canvas>
+            <Pie data={this.state.audienceData} options={{}} />
           </div>
         </div>
         <div className="row p-4">
           <div className="col card">
-            <canvas id="chart" style={{display: 'block', width: 455, height: 205}}>
-  </canvas>
+            <Bar data={this.state.chartData} options={{}} />
           </div>
         </div>
       </div>
-    )
+    );
+  }
 }
