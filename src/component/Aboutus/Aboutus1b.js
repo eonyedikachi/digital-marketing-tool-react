18c512aed4ac0a7115e46dfd97d9f568b9a146c1
Osaiteh <ogloriousgloo@gmail.com>
2021-01-21 12:11:22 +0100

    Update Aboutus1b.js

--- src/component/Aboutus/Aboutus1b.js
+++ src/component/Aboutus/Aboutus1b.js
@@ -3,12 +3,22 @@ import "./Aboutus1b.css"
 
  function About1b(props) {
     return (
-        <div className="card Abtuscard margin-bottom">
-        <h5 className="mission"><p>{props.mission}</p></h5>
+      <div className="card Abtuscard margin-bottom">
+        <h5 className="mission">
+          <p>{props.mission}</p>
+        </h5>
         <div className="carddiv missioninfo ">
-          <p>{props.missioninfo}</p>
+          <p className="ml-1">
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{props.missioninfo}
+          </p>
+          <p>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{props.missioninf}
+          </p>
+          <p>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{props.missionin}
+          </p>
         </div>
       </div>
-    )
+    );
 }
 export default About1b;
